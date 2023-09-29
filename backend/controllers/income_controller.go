d1f0e015461f90b22f12179f7749adbe02926b95
farizibnu <fariz2240@gmail.com>
2023-09-29 14:01:55 +0700

    add income and budget

---
+++ backend/controllers/income_controller.go
@@ -0,0 +1,195 @@
+package controllers
+
+import (
+	"github.com/finance-management/models"
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+type IncomeController struct {
+	DB *gorm.DB
+}
+
+func NewIncomeController(db *gorm.DB) *IncomeController {
+	return &IncomeController{DB: db}
+}
+
+// CreateIncome handles the creation of a new income.
+func (controller *IncomeController) CreateIncome(c *fiber.Ctx) error {
+	var income models.Income
+	if err := c.BodyParser(&income); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	// Save the income to the database using GORM
+	controller.DB.Create(&income)
+
+	return c.Status(fiber.StatusCreated).JSON(income)
+}
+
+// CreateOutcome handles the creation of a new outcome.
+func (controller *IncomeController) CreateOutcome(c *fiber.Ctx) error {
+	var outcome models.Outcome
+	if err := c.BodyParser(&outcome); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	// Save the outcome to the database using GORM
+	controller.DB.Create(&outcome)
+
+	return c.Status(fiber.StatusCreated).JSON(outcome)
+}
+
+// // CreateWallet handles the creation of a new wallet.
+// func (controller *IncomeController) CreateWallet(c *fiber.Ctx) error {
+// 	var wallet models.Wallet
+// 	if err := c.BodyParser(&wallet); err != nil {
+// 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+// 	}
+
+// 	// Save the wallet to the database using GORM
+// 	controller.DB.Create(&wallet)
+
+// 	return c.Status(fiber.StatusCreated).JSON(wallet)
+// }
+
+// GetIncomeByID retrieves an income by its ID.
+func (controller *IncomeController) GetIncomeByID(c *fiber.Ctx) error {
+	incomeID := c.Params("id")
+
+	var income models.Income
+	if err := controller.DB.Where("id = ?", incomeID).First(&income).Error; err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Income not found"})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(income)
+}
+
+// GetOutcomeByID retrieves an outcome by its ID.
+func (controller *IncomeController) GetOutcomeByID(c *fiber.Ctx) error {
+	outcomeID := c.Params("id")
+
+	var outcome models.Outcome
+	if err := controller.DB.Where("id = ?", outcomeID).First(&outcome).Error; err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Outcome not found"})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(outcome)
+}
+
+// // GetWalletByID retrieves a wallet by its ID.
+// func (controller *IncomeController) GetWalletByID(c *fiber.Ctx) error {
+// 	walletID := c.Params("id")
+
+// 	var wallet models.Wallet
+// 	if err := controller.DB.Where("id = ?", walletID).First(&wallet).Error; err != nil {
+// 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Wallet not found"})
+// 	}
+
+// 	return c.Status(fiber.StatusOK).JSON(wallet)
+// }
+
+// UpdateIncome updates an existing income.
+func (controller *IncomeController) UpdateIncome(c *fiber.Ctx) error {
+	incomeID := c.Params("id")
+
+	var updatedIncome models.Income
+	if err := c.BodyParser(&updatedIncome); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	var income models.Income
+	if err := controller.DB.Where("id = ?", incomeID).First(&income).Error; err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Income not found"})
+	}
+
+	// Update the income in the database
+	controller.DB.Model(&income).Updates(&updatedIncome)
+
+	return c.Status(fiber.StatusOK).JSON(income)
+}
+
+// UpdateOutcome updates an existing outcome.
+func (controller *IncomeController) UpdateOutcome(c *fiber.Ctx) error {
+	outcomeID := c.Params("id")
+
+	var updatedOutcome models.Outcome
+	if err := c.BodyParser(&updatedOutcome); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	var outcome models.Outcome
+	if err := controller.DB.Where("id = ?", outcomeID).First(&outcome).Error; err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Outcome not found"})
+	}
+
+	// Update the outcome in the database
+	controller.DB.Model(&outcome).Updates(&updatedOutcome)
+
+	return c.Status(fiber.StatusOK).JSON(outcome)
+}
+
+// // UpdateWallet updates an existing wallet.
+// func (controller *IncomeController) UpdateWallet(c *fiber.Ctx) error {
+// 	walletID := c.Params("id")
+
+// 	var updatedWallet models.Wallet
+// 	if err := c.BodyParser(&updatedWallet); err != nil {
+// 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+// 	}
+
+// 	var wallet models.Wallet
+// 	if err := controller.DB.Where("id = ?", walletID).First(&wallet).Error; err != nil {
+// 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Wallet not found"})
+// 	}
+
+// 	// Update the wallet in the database
+// 	controller.DB.Model(&wallet).Updates(&updatedWallet)
+
+// 	return c.Status(fiber.StatusOK).JSON(wallet)
+// }
+
+// DeleteIncome deletes an existing income.
+func (controller *IncomeController) DeleteIncome(c *fiber.Ctx) error {
+	incomeID := c.Params("id")
+
+	var income models.Income
+	if err := controller.DB.Where("id = ?", incomeID).First(&income).Error; err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Income not found"})
+	}
+
+	// Delete the income from the database
+	controller.DB.Delete(&income)
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": "Income deleted successfully"})
+}
+
+// DeleteOutcome deletes an existing outcome.
+func (controller *IncomeController) DeleteOutcome(c *fiber.Ctx) error {
+	outcomeID := c.Params("id")
+
+	var outcome models.Outcome
+	if err := controller.DB.Where("id = ?", outcomeID).First(&outcome).Error; err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Outcome not found"})
+	}
+
+	// Delete the outcome from the database
+	controller.DB.Delete(&outcome)
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": "Outcome deleted successfully"})
+}
+
+// // DeleteWallet deletes an existing wallet.
+// func (controller *IncomeController) DeleteWallet(c *fiber.Ctx) error {
+// 	walletID := c.Params("id")
+
+// 	var wallet models.Wallet
+// 	if err := controller.DB.Where("id = ?", walletID).First(&wallet).Error; err != nil {
+// 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Wallet not found"})
+// 	}
+
+// 	// Delete the wallet from the database
+// 	controller.DB.Delete(&wallet)
+
+// 	return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": "Wallet deleted successfully"})
+// }
--- backend/main.go
+++ backend/main.go
@@ -30,27 +30,41 @@ func main() {
 	// Initialize the UserController with the database
 	userController := controllers.NewUserController(db)
 	walletController := controllers.NewWalletController(db)
-	outcomeController := controllers.NewOutcomeController(db)
-	dailyRecapController := controllers.NewDailyRecapController(db)
+
+	// 	outcomeController := controllers.NewOutcomeController(db)
+	// 	dailyRecapController := controllers.NewDailyRecapController(db)
+	budgetController := controllers.NewBudgetController(db)
+	incomeController := controllers.NewIncomeController(db)
 
 	// Define a route to get user data
 	app.Post("/api/user/register", userController.RegisterUser)
 	app.Get("/api/user/find", userController.GetUser)
 	app.Get("/api/users", userController.GetUsers)
 	app.Post("/api/user/token", userController.UpdateToken)
+
 	app.Post("/api/wallet/new", walletController.CreateWallet)
 	app.Get("/api/wallet/:id", walletController.GetWallet)
 	app.Put("/api/wallet/:id", walletController.UpdateWallet)
 
-	app.Post("/api/outcome/new", outcomeController.CreateOutcome)
-	app.Get("/api/outcome/:id", outcomeController.GetOutcome)
-	app.Put("/api/outcome/:id", outcomeController.UpdateOutcome)
-	app.Delete("/api/outcome/delete", outcomeController.DeleteOutcome)
+	// 	app.Post("/api/outcome/new", outcomeController.CreateOutcome)
+	// 	app.Get("/api/outcome/:id", outcomeController.GetOutcome)
+	// 	app.Put("/api/outcome/:id", outcomeController.UpdateOutcome)
+	// 	app.Delete("/api/outcome/delete", outcomeController.DeleteOutcome)
 
-	app.Post("/api/dailyrecap/new", dailyRecapController.CreateDailyRecap)
-	app.Get("/api/dailyrecap/:id", dailyRecapController.GetDailyRecap)
-	app.Put("/api/dailyrecap/:id", dailyRecapController.UpdateDailyRecap)
-	app.Delete("/api/dailyrecap/delete/:id", dailyRecapController.DeleteDailyRecap)
+	// 	app.Post("/api/dailyrecap/new", dailyRecapController.CreateDailyRecap)
+	// 	app.Get("/api/dailyrecap/:id", dailyRecapController.GetDailyRecap)
+	// 	app.Put("/api/dailyrecap/:id", dailyRecapController.UpdateDailyRecap)
+	// 	app.Delete("/api/dailyrecap/delete/:id", dailyRecapController.DeleteDailyRecap)
+
+	app.Post("/api/budget/new", budgetController.CreateBudget)
+	app.Get("/api/budget/:id", budgetController.GetBudgetByID)
+	app.Put("/api/budget/:id", budgetController.UpdateBudget)
+	app.Delete("/api/budget/delete/:id", budgetController.DeleteBudget)
+
+	app.Post("/api/income", incomeController.CreateIncome)
+	app.Get("/api/income/:id", incomeController.GetIncomeByID)
+	app.Put("/api/income/:id", incomeController.UpdateIncome)
+	app.Delete("/api/income/:id", incomeController.DeleteIncome)
 
 	// Start the server
 	app.Listen(":5000")
