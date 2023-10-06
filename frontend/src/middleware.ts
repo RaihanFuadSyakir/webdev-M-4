189cf44498864c27273d349bd0bc0c5bec20ff57
NItroF77 <142875762+RaihanFuadSyakir@users.noreply.github.com>
2023-10-06 17:59:13 +0700

    update login page and fixed middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -3,9 +3,8 @@ import { NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-    console.log("kena middleware");
     if (request.cookies.has("token")) {
-        if (request.nextUrl.pathname.startsWith('/login') || request.nextUrl.pathname.startsWith('/register')) {
+        if (request.nextUrl.pathname.endsWith('/login') || request.nextUrl.pathname.endsWith('/register')) {
             return NextResponse.redirect(new URL('/dashboard', request.url));
         }
         return NextResponse.next()
@@ -18,5 +17,5 @@ export function middleware(request: NextRequest) {
 }
 // See "Matching Paths" below to learn more
 export const config = {
-    matcher: ['/:path*'],
+    matcher: ['/'],
 }
