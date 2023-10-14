cabda325f77d462eb6d6fa777846eb4eb59fe663
NItroF77 <142875762+RaihanFuadSyakir@users.noreply.github.com>
2023-10-14 15:37:40 +0700

    added trigger for income,outcome update wallet

--- backend/main.go
+++ backend/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/finance-management/controllers"
 	"github.com/finance-management/middleware"
+	"github.com/finance-management/migrations"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	// Update the import path
@@ -27,16 +28,18 @@ func main() {
 		panic("Failed to connect to the database")
 	}
 
-	/* // Apply migrations
-	if err := migrations.Delete(db); err != nil {
-		panic("Failed to Delete Tables")
+	// // Apply migrations
+	// if err := migrations.Delete(db); err != nil {
+	// 	panic("Failed to Delete Tables")
+	// }
+	// if err := migrations.Migrate(db); err != nil {
+	// 	panic("Failed to apply migrations")
+	// }
+	if err := migrations.AddTriggers(db); err != nil {
+		panic("Failed to ad triggers")
 	}
-	if err := migrations.Migrate(db); err != nil {
-		panic("Failed to apply migrations")
-	}
-
-	// Seed the database with dummy data
-	seeds.Seed(db) // Update the function call with the correct path */
+	// // Seed the database with dummy data
+	// seeds.Seed(db) // Update the function call with the correct path
 
 	// Initialize the UserController with the database
 
