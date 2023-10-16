ce034af7eae7de366e6ca99df1dd71659055ce95
NItroF77 <142875762+RaihanFuadSyakir@users.noreply.github.com>
2023-10-16 08:51:52 +0700

    temp update category

--- frontend/src/components/category/ListCategory.tsx
+++ frontend/src/components/category/ListCategory.tsx
@@ -11,46 +11,32 @@ import TableCell from '@mui/material/TableCell';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-const ListCategories = ({seed} : {seed: number}) => {
-    const [Categories, setCategories] = useState<Category[]>([]);
-      
-    // Fetch outcomes data when the component mounts
-    useEffect(() => {
-      axiosInstance
-        .get(`${BACKEND_URL}/api/categories/user`) // Replace with your actual endpoint
-        .then((response : AxiosResponse<dbResponse<Category[]>>) => {
-          const res : dbResponse<Category[]> = response.data;
-          console.log(res,"db")
-          setCategories(res.data);
-          console.log(Categories);
-        })
-        .catch((error) => {
-          console.error('Failed to fetch categories:', error);
-        });
-    }, [seed]);
-  
-    return (
-      <div className='max-w-2xl'>
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell align="center">Category Name</TableCell>
+interface CategoryProps {
+  categories: Category[]
+}
+const ListCategories = ({ categories }: CategoryProps) => {
+
+  return (
+    <div className='max-w-2xl'>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Category Name</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {categories.map((category) => (
+              <TableRow key={category.id}>
+
+                <TableCell component="th" scope="row">{category.category_name}</TableCell>
               </TableRow>
-            </TableHead>
-            <TableBody>
-              {Categories.map((category) => (
-                <TableRow key={category.id}>
-  
-                  <TableCell component="th" scope="row">{category.category_name}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </div>
-    );
-  };
-  
-  export default ListCategories;
-  
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+};
+
+export default ListCategories;
