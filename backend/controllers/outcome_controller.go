dd97d900e959dc635be96af954be7cf9a34f30a7
Annisa Dinda <100127500+annisaadg@users.noreply.github.com>
2023-11-16 18:44:38 +0700

    Update outcome_controller.go

--- backend/controllers/outcome_controller.go
+++ backend/controllers/outcome_controller.go
@@ -17,32 +17,84 @@ func NewOutcomeController(db *gorm.DB) *OutcomeController {
 	return &OutcomeController{DB: db}
 }
 
+func (oc *OutcomeController) isBalanceSufficient(walletID uint, totalOutcome float64) (bool, error) {
+	var wallet models.Wallet
+	if err := oc.DB.First(&wallet, walletID).Error; err != nil {
+		return false, err
+	}
+
+	return wallet.TotalBalance >= totalOutcome, nil
+}
+
 func (oc *OutcomeController) CreateOutcome(c *fiber.Ctx) error {
 	var outcome models.Outcome
 	if err := c.BodyParser(&outcome); err != nil {
-		fmt.Println("outcome 0")
 		return jsonResponse(c, fiber.StatusBadRequest, "Bad Request", nil)
 	}
+
 	userID := c.Locals("userID")
 	outcome.UserID = userID.(uint)
-	// You might want to authenticate the user here and set the UserID accordingly.
-	// wallet.UserID = authenticatedUserID
+
+	if sufficient, err := oc.isBalanceSufficient(outcome.WalletID, outcome.TotalOutcome); err != nil {
+		return jsonResponse(c, fiber.StatusInternalServerError, "Internal Server Error", nil)
+	} else if !sufficient {
+		return jsonResponse(c, fiber.StatusBadRequest, "Insufficient balance", nil)
+	}
 
 	if err := oc.DB.Create(&outcome).Error; err != nil {
 		return jsonResponse(c, fiber.StatusInternalServerError, "Internal Server Error", nil)
 	}
-	fmt.Println("Outcome created successfully")
-	// Preload the Category and Wallet associations before returning the outcome
+
 	if err := oc.DB.Preload("Category").Preload("Wallet").First(&outcome, outcome.ID).Error; err != nil {
 		return jsonResponse(c, fiber.StatusInternalServerError, "Internal Server Error", nil)
 	}
-	// Call the function to calculate and update CurrentBudget
+
 	if err := oc.calculateAndUpdateBudgetCurrentBudget(outcome.CategoryID, outcome.Date); err != nil {
-		// Handle error
 		fmt.Println("Error updating CurrentBudget:", err)
 		return jsonResponse(c, fiber.StatusInternalServerError, "Internal Server Error", nil)
 	}
+
 	return jsonResponse(c, fiber.StatusCreated, "Outcome created successfully", outcome)
+}
+
+func (oc *OutcomeController) UpdateOutcome(c *fiber.Ctx) error {
+	var outcome models.Outcome
+	outcomeID := c.Params("id")
+
+	if err := oc.DB.First(&outcome, outcomeID).Error; err != nil {
+		return jsonResponse(c, fiber.StatusNotFound, "Outcome not found", nil)
+	}
+
+	oldCategoryID := outcome.CategoryID
+	oldDate := outcome.Date
+
+	if err := c.BodyParser(&outcome); err != nil {
+		return jsonResponse(c, fiber.StatusBadRequest, err.Error(), nil)
+	}
+
+	if sufficient, err := oc.isBalanceSufficient(outcome.WalletID, outcome.TotalOutcome); err != nil {
+		return jsonResponse(c, fiber.StatusInternalServerError, "Internal Server Error", nil)
+	} else if !sufficient {
+		return jsonResponse(c, fiber.StatusBadRequest, "Insufficient balance", nil)
+	}
+
+	if err := oc.DB.Save(&outcome).Error; err != nil {
+		return jsonResponse(c, fiber.StatusInternalServerError, "Internal Server Error", nil)
+	}
+
+	if err := oc.calculateAndUpdateBudgetCurrentBudget(oldCategoryID, oldDate); err != nil {
+		fmt.Println("Error updating CurrentBudget:", err)
+	}
+
+	if err := oc.calculateAndUpdateBudgetCurrentBudget(outcome.CategoryID, outcome.Date); err != nil {
+		fmt.Println("Error updating CurrentBudget:", err)
+	}
+
+	if err := oc.DB.Preload("Wallet").Preload("Category").First(&outcome, outcome.ID).Error; err != nil {
+		return jsonResponse(c, fiber.StatusInternalServerError, "Internal Server Error", nil)
+	}
+
+	return jsonResponse(c, fiber.StatusOK, "Outcome updated successfully", outcome)
 }
 
 // GetOutcome retrieves a single outcome by its ID.
@@ -75,45 +127,6 @@ func (oc *OutcomeController) GetOutcomeByUserID(c *fiber.Ctx) error {
 		}
 	}
 	return jsonResponse(c, fiber.StatusOK, "OK", user.Outcomes)
-}
-
-// UpdateOutcome updates an existing outcome by its ID.
-func (oc *OutcomeController) UpdateOutcome(c *fiber.Ctx) error {
-	var outcome models.Outcome
-	outcomeID := c.Params("id")
-
-	if err := oc.DB.First(&outcome, outcomeID).Error; err != nil {
-		return jsonResponse(c, fiber.StatusNotFound, "Outcome not found", nil)
-	}
-
-	// Backup the old categoryID and date
-	oldCategoryID := outcome.CategoryID
-	oldDate := outcome.Date
-
-	if err := c.BodyParser(&outcome); err != nil {
-		return jsonResponse(c, fiber.StatusBadRequest, err.Error(), nil)
-	}
-
-	if err := oc.DB.Save(&outcome).Error; err != nil {
-		return jsonResponse(c, fiber.StatusInternalServerError, "Internal Server Error", nil)
-	}
-
-	// Update CurrentBudget for old categoryID and old date
-	if err := oc.calculateAndUpdateBudgetCurrentBudget(oldCategoryID, oldDate); err != nil {
-		fmt.Println("Error updating CurrentBudget:", err)
-		// Handle error if needed
-	}
-
-	// Update CurrentBudget for the new categoryID and new date
-	if err := oc.calculateAndUpdateBudgetCurrentBudget(outcome.CategoryID, outcome.Date); err != nil {
-		fmt.Println("Error updating CurrentBudget:", err)
-		// Handle error if needed
-	}
-
-	if err := oc.DB.Preload("Wallet").Preload("Category").First(&outcome, outcome.ID).Error; err != nil {
-		return jsonResponse(c, fiber.StatusInternalServerError, "Internal Server Error", nil)
-	}
-	return jsonResponse(c, fiber.StatusOK, "Outcome updated successfully", outcome)
 }
 
 // DeleteOutcome deletes an existing outcome by its ID.
