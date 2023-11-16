6fb3d752cc686378d7f8360815f9e491d89bace3
Muhammad Deo Audha Rizki <muhammad.deo.tif421@polban.ac.id>
2023-11-16 08:41:29 +0700

    Add Offline Support

--- frontend/worker/index.ts
+++ frontend/worker/index.ts
@@ -1,38 +1,76 @@
 import { util } from './util';
 
-declare let self: ServiceWorkerGlobalScope
+declare let self: ServiceWorkerGlobalScope;
 
-// To disable all workbox logging during development, you can set self.__WB_DISABLE_DEV_LOGS to true
-// https://developers.google.com/web/tools/workbox/guides/configure-workbox#disable_logging
-//
-// self.__WB_DISABLE_DEV_LOGS = true
+const CACHE_NAME = 'my-cache-v2'; // Update the cache name for versioning
 
 util();
 
+let cacheData = "appV1";
+
 self.addEventListener('install', (event) => {
     event?.waitUntil(
-      caches.open('my-cache').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/finance-management/login',
-          '/finance-management/register',
-          '/finance-management/budget',
-          '/finance-management/categories',
-          '/finance-management/dashboard',
-          '/finance-management/incomes',
-          '/finance-management/outcomes',
-          '/finance-management/recap',
-          '/finance-management/report',
-          '/finance-management/wallets',
-        ]);
-      })
+        caches.open(cacheData).then((cache) => {
+            return cache.addAll([
+                '/',
+                '/finance-management/login',
+                '/finance-management/register',
+                '/finance-management/budget',
+                '/finance-management/categories',
+                '/finance-management/dashboard',
+                '/finance-management/incomes',
+                '/finance-management/outcomes',
+                '/finance-management/recap',
+                '/finance-management/report',
+                '/finance-management/wallets',
+                '/manifest.json', // Include the manifest file in the cache
+            ]);
+        })
     );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event?.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
+});
+
+self.addEventListener('fetch', (event) => {
+    if (event?.request.method !== 'GET') {
+        return; // Do not cache non-GET requests
+    }
+
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            // Return the cached response if it exists
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+
+            // For other requests, continue fetching from the network
+            return fetch(event.request).then((fetchResponse) => {
+                // Check if we received a valid response
+                if (!fetchResponse || fetchResponse.status !== 200 || fetchResponse.type !== 'basic') {
+                    return fetchResponse;
+                }
+
+                // Clone the response to store it in the cache
+                const responseToCache = fetchResponse.clone();
+
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, responseToCache);
+                });
+
+                return fetchResponse;
+            });
+        })
     );
-  });
+});
+
+self.addEventListener('activate', (event) => {
+    event?.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((name) => {
+                    if (name !== CACHE_NAME && name.startsWith("app")) {
+                        return caches.delete(name);
+                    }
+                })
+            );
+        })
+    );
+});
