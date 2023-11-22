a23e82682f7fc6e3cf692e868dc8b0c0a1d77f8f
Muhammad Deo Audha Rizki <muhammad.deo.tif421@polban.ac.id>
2023-11-23 06:01:58 +0700

    Fix Backend

--- backend/controllers/outcome_controller.go
+++ backend/controllers/outcome_controller.go
@@ -25,25 +25,48 @@ func (oc *OutcomeController) CreateOutcome(c *fiber.Ctx) error {
 	}
 	userID := c.Locals("userID")
 	outcome.UserID = userID.(uint)
-	// You might want to authenticate the user here and set the UserID accordingly.
-	// wallet.UserID = authenticatedUserID
+
+	// Check if the outcome exceeds the total balance in the selected wallet
+	wallet, err := oc.getWalletByID(outcome.WalletID)
+	if err != nil {
+		fmt.Println("Error getting wallet:", err)
+		return jsonResponse(c, fiber.StatusInternalServerError, "Internal Server Error", nil)
+	}
+
+	if outcome.TotalOutcome > wallet.TotalBalance {
+		return jsonResponse(c, fiber.StatusBadRequest, "Outcome exceeds total balance in the selected wallet", nil)
+	}
 
 	if err := oc.DB.Create(&outcome).Error; err != nil {
 		return jsonResponse(c, fiber.StatusInternalServerError, "Internal Server Error", nil)
 	}
+
 	fmt.Println("Outcome created successfully")
+
 	// Preload the Category and Wallet associations before returning the outcome
 	if err := oc.DB.Preload("Category").Preload("Wallet").First(&outcome, outcome.ID).Error; err != nil {
 		return jsonResponse(c, fiber.StatusInternalServerError, "Internal Server Error", nil)
 	}
+
 	// Call the function to calculate and update CurrentBudget
 	if err := oc.calculateAndUpdateBudgetCurrentBudget(outcome.CategoryID, outcome.Date); err != nil {
 		// Handle error
 		fmt.Println("Error updating CurrentBudget:", err)
 		return jsonResponse(c, fiber.StatusInternalServerError, "Internal Server Error", nil)
 	}
+
 	return jsonResponse(c, fiber.StatusCreated, "Outcome created successfully", outcome)
 }
+
+// getWalletByID retrieves a wallet by ID.
+func (oc *OutcomeController) getWalletByID(walletID uint) (models.Wallet, error) {
+	var wallet models.Wallet
+	if err := oc.DB.First(&wallet, walletID).Error; err != nil {
+		return wallet, err
+	}
+	return wallet, nil
+}
+
 
 // GetOutcome retrieves a single outcome by its ID.
 func (oc *OutcomeController) GetOutcome(c *fiber.Ctx) error {
